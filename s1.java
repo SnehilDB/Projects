6572bb765ae53bbcec5505d390e821a36147108a
SnehilDB <51693981+SnehilDB@users.noreply.github.com>
2019-06-18 13:23:51 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,17 @@
+package s1;
+
+public class s1
+{
+    public static void main(String[] args)
+    {
+        for(int i=0;i<5;i++)
+        {
+            for(int j=i+1;j<5;j++)
+            {
+
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
